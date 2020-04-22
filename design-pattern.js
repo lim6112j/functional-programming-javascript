221f01fff488fe7831f4a82b52bb0fab8be7a855
byeong cheol lim <byeongcheollim@byeongui-MacBookPro.local>
2020-04-23 04:38:16 +0900

    monad made

--- design-pattern.js
+++ design-pattern.js
@@ -1,6 +1,8 @@
 // functor, monad
 import _ from 'lodash'
 import * as R from 'ramda'
+import Maybe from './monad/Maybe'
+import util from 'util'
 const log = (val) => console.log(val)
 // compare functor - map , monad - flatMap design pattern
 const arr = [1,2,3,4,5]
@@ -15,3 +17,46 @@ log('null value mapping ', arr4)
 // ramda identity function
 const { identity } = R
 R.map(identity, [1,2,3]).map(log)
+
+// monad maybe
+const num = Maybe.of(4)
+const nullval = Maybe.nothing()
+console.log(num.toString())
+console.log(num.map(x=> x+2).value)
+console.log(nullval.map(x=>x+2).getOrElse('any'))
+
+// member.address.country.countryCode
+const members = [
+  {
+    name: 'lim',
+    address: {
+      country: {
+        countryCode: 'kr'
+      }
+    }
+  }
+]
+
+const memberMaybe = Maybe.of(members[0])
+console.log(`memberMaybe => ${util.inspect(memberMaybe, true, 10, true)}`)
+
+// imperative
+function getCountry(member) {
+  let address = member.address
+  if(address !== null) {
+    let country = address.country
+    if(country !== null) {
+      return country.countryCode
+    }
+  }
+  return '존재하지 않는 국가'
+}
+// functional with maybe
+const getCountryMaybe = (member) => member
+  .map(R.prop('address'))
+  .map(R.prop('country'))
+  .map(R.prop('countryCode'))
+  .getOrElse('존재하지 않는 국가')
+
+console.log(`imperative => ${getCountry(members[0])}`)
+console.log(`functional => ${getCountryMaybe(memberMaybe)}`)
---
+++ monad/Maybe.js
@@ -0,0 +1,64 @@
+export default class Maybe {
+  static just(a) {
+    return new Just(a)
+  }
+  static nothing() {
+    return new Nothing()
+  }
+  static fromNullable(a) {
+    return a !== null ? Maybe.just(a) : Maybe.nothing()
+  }
+  static of(a) {
+    return this.just(a)
+  }
+  get isNothing() {
+    return false
+  }
+  get isJust() {
+    return false
+  }
+}
+class Just extends Maybe {
+  constructor(value) {
+    super();
+    this._value = value
+  }
+  get value() {
+    return this._value
+  }
+  map(f) {
+    return Maybe.fromNullable(f(this._value))
+  }
+  getOrElse() {
+    return this._value
+  }
+  filter(f) {
+    Maybe.fromNullable(f(this._value) ? this._value : null)
+  }
+  chain(f) {
+    return f(this._value)
+  }
+  toString() {
+    return `Maybe.Just(${this._value})`
+  }
+}
+class Nothing extends Maybe {
+  map(f) {
+    return this
+  }
+  get value() {
+    throw new TypeError("Nothing 값을 가져올 수 없습니다.")
+  }
+  getOrElse(other) {
+    return other
+  }
+  filter(f) {
+    return this._value
+  }
+  chain(f) {
+    return this
+  }
+  toString() {
+    return `Maybe.Nothing`
+  }
+}
