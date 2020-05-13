a3b651dda6a955295d5849efe3730cf809d1523f
lim byeong cheol <lim6112j@gmail.com>
2020-05-13 19:15:19 +0900

    memoized finished

--- memoized.js
+++ memoized.js
@@ -11,20 +11,28 @@ function expensiveFunc(a, memoized) {
   syncWait(3000)
   return expensiveFunc.cache.a = a * 34
 }
-
-const call1 = expensiveFunc(6, true);
+console.log('Started ....')
+const call1 = expensiveFunc(5, true);
 log('call1')(call1)
-const call2 = expensiveFunc(6, true);
+const call2 = expensiveFunc(5, true);
 log('call2')(call2)
-const call3 = expensiveFunc(6, true);
+const call3 = expensiveFunc(5, true);
 log('call3')(call3)
 
 function memoized(fn) {
   return function() {
     var args = Array.prototype.slice.call(arguments);
     console.log(args)
-    return fn.apply(this, args)
+    fn.cache = fn.cache || {}
+    console.log(JSON.stringify(fn.cache))
+    return fn.cache[args] ? fn.cache[args] : (fn.cache[args] = fn.apply(this, args))
   }
 }
 
-log('memoized func')(memoized(expensiveFunc)(6, true))
+log('memoized func')(memoized(expensiveFunc)(6, false))
+log('memoized func')(memoized(expensiveFunc)(6, false))
+log('memoized func')(memoized(expensiveFunc)(6, false))
+const arr = [1,2,3];
+const obj = {};
+obj[arr] = 4;
+console.log(JSON.stringify(obj))
