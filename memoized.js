bee6d4c89d2e1e7af6b6befaaae674ba8ebac69d
lim byeong cheol <lim6112j@gmail.com>
2020-05-14 18:51:12 +0900

    IIFE added

--- memoized.js
+++ memoized.js
@@ -1,5 +1,4 @@
 import _ from 'lodash'
-import assert from 'assert';
 const log = (msg) => (v) => console.log(msg ,' => ', v)
 // const syncWait = ms => {
 //   const end = Date.now() + ms
@@ -21,7 +20,7 @@ const log = (msg) => (v) => console.log(msg ,' => ', v)
 // const call3 = expensiveFunc(5, true);
 // log('call3')(call3)
 
-// function memoized(fn) {
+// function memoized (fn) {
 //   return function() {
 //     var args = Array.prototype.slice.call(arguments);
 //     console.log(args)
@@ -39,60 +38,84 @@ const log = (msg) => (v) => console.log(msg ,' => ', v)
 // obj[arr] = 4;
 // console.log(JSON.stringify(obj))
 
-const func = function (a, b, c) {
-  // const args = Array.prototype.slice.call(arguments)
-  log('arguments')(arguments)
-  log('arguments lenght')(arguments.length)
-  log('this')(this)
-}
-const obj = {
-  name: 'kosac',
-  id: 1,
-  method: func
-}
-func(1,2,3,4,5)
-obj.method(1,2,3)
+// const func = function (a, b, c) {
+//   // const args = Array.prototype.slice.call(arguments)
+//   log('arguments')(arguments)
+//   log('arguments lenght')(arguments.length)
+//   log('this')(this)
+// }
+// const obj = {
+//   name: 'kosac',
+//   id: 1,
+//   method: func
+// }
+// func(1,2,3,4,5)
+// obj.method(1,2,3)
 
-// const funcinstance = new func();
-new func(1,2,4) // invoking as constructor
+// // const funcinstance = new func();
+// new func(1,2,4) // invoking as constructor
 
-const funcCon = function() {
-  return function() {
-    log('constructor')(this);
-  };
-}
-const funcInner = new funcCon()
-funcInner(1,2,3)
+// const funcCon = function() {
+//   return function() {
+//     log('constructor')(this);
+//   };
+// }
+// const funcInner = new funcCon()
+// funcInner(1,2,3)
 
-const funcCon2 = function() {
-  log('arguments in funccon2')(arguments)
-  this.innerFunc = function() {
-    log('arguments in inner constructor')(arguments)
-    return this;
-  }
-}
+// const funcCon2 = function() {
+//   log('arguments in funccon2')(arguments)
+//   this.innerFunc = function() {
+//     log('arguments in inner constructor')(arguments)
+//     return this;
+//   }
+// }
 
-const funcInner2 = new funcCon2();
-log('funcinner2')(funcInner2.innerFunc(1,2,3))
+// const funcInner2 = new funcCon2();
+// log('funcinner2')(funcInner2.innerFunc(1,2,3))
 
-const funcCon3 = function() {
-  return this.inner = function() {
-    log('funcCon3')(arguments)
-    return this;
-  }
-}
+// const funcCon3 = function() {
+//   return this.inner = function() {
+//     log('funcCon3')(arguments)
+//     return this;
+//   }
+// }
 
-const innerFunc3 = new funcCon3();
-log('funcCon3')(innerFunc3(1,2,3,4,5,6))
+// const innerFunc3 = new funcCon3();
+// log('funcCon3')(innerFunc3(1,2,3,4,5,6))
 
-function kosac() {
-  this.member = function() {
-    return this;
-  }
-}
+// function kosac() {
+//   this.member = function() {
+//     return this;
+//   }
+// }
 
 // const aKosac = kosac(); // this === window in non-strict mode; this === undefined in strict mode
 // log('aKosac')(aKosac)
-const newKosac = new kosac();
-log('newKosac')(newKosac.member())
-assert(true, 'asset success')
+// const newKosac = new kosac();
+// log('newKosac')(newKosac.member())
+// console.assert(true, 'asset success')
+
+// const Csrt = function() {
+//   this.memberMethod = function() {
+//     console.log(this)
+//     return this;
+//   }
+//   return 1;
+// }
+// // run on jsbin.com below in not strict mode
+// // log('csrt')(csrt())
+// const csrtIns = new Csrt();
+// log('this(of csrt function)')(csrtIns.memberMethod())
+
+const IIFE = (function() {
+  const obj = {
+    name: 'lim',
+    method: () => {
+      console.log(this)
+      return this
+    }
+  }
+  return obj;
+})();
+log('iife')(IIFE.method())
