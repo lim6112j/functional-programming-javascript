0b438fc6d90a8ef925a3aad217de72085920e37e
lim byeong cheol <lim6112j@gmail.com>
2020-05-15 12:04:46 +0900

    this test in constructor made

--- functionAsConstructor/index.ts
+++ functionAsConstructor/index.ts
@@ -1,12 +1,24 @@
 'use strict'
+const log = (msg, v) => console.log(msg, " => ", v);
+function assert(value, desc) {
+  const li = document.createElement("li");
+  li.className = value ? "pass" : "fail";
+  li.appendChild(document.createTextNode(desc));
+  document.getElementById("results").appendChild(li);
+}
 function Button() {
   this.clicked = false;
   this.click = function() {
-    console.log('clicked')
     this.clicked = true;
-    console.log(this.clicked)
-  }
+    assert(button.clicked, "button clicked");
+    assert(button instanceof Button, "button is instance of Button constructor");
+    assert(this instanceof Button, "this is instance of Button constructor")
+    log("this", this)
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(button.clicked))
+    document.getElementById("results").appendChild(li);}
 }
-var button = new Button();
-var elem = document.getElementById("test");
+const button = new Button();
+const elem = document.getElementById("test");
 elem.addEventListener('click', button.click);
+
