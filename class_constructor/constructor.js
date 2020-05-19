558ba44efe50af4963612f809f3c8b8774885ab5
lim byeong cheol <lim6112j@gmail.com>
2020-05-19 18:51:11 +0900

    inv as constructor , funciton, class etc

--- class_constructor/constructor.js
+++ class_constructor/constructor.js
@@ -1,24 +1,79 @@
+import {log, logl, proLog} from '../utils';
+import { CombineLatestOperator } from 'rxjs/internal/observable/combineLatest';
 // use external class function as property
+// class Engine {
+//   start() {
+//     console.log('Started');
+//   }
+//   stop() {
+//     console.log('engine stop');
+//   }
+// }
+// class Car {
+//   constructor(/* private engine: Engine */) {
+//     this.engine = new Engine();
+//   }
+//   // function as property
+//   start() {
+//     this.engine.start()
+//   }
+//   // start = this.engine.start;
+//   // stop = this.engine.stop
+// }
+
+// const car = new Car();
+// // const car = new Car(new Engine());
+// car.start()
+// var later;
+// function Engine () {
+//   function drive() { console.log('drive')}
+//   // later = drive
+//   this.start = () => {
+//     console.log('start');
+//   }
+//   this.stop = function() {
+//     console.log(this)
+//   }
+// }
+// const engine = new Engine();
+// logl('engine')(engine)
+// Engine()
+// later()
+// class Engine {
+//   start = () => {
+//     console.log('Started');
+//   }
+//   stop() {
+//     console.log('engine stop');
+//   }
+// }
+// class Car {
+//   constructor(engine) {
+//     this.engine = engine;
+//   }
+//   // function as property
+//   start() {
+//     this.engine.start()
+//   }
+//   stop = () => {console.log('stop')}
+//   // start = this.engine.start;
+//   // stop = this.engine.stop
+// }
+// const car = new Car(new Engine());
+// car.start()
+// car.stop()
+
+
 class Engine {
-  start() {
-    console.log('Started');
-  }
+  static drive = () => {}
   stop() {
     console.log('engine stop');
   }
-}
-class Car {
-  constructor(/* private engine: Engine */) {
-    this.engine = new Engine();
+  get start ()  {
+    console.log('engine starts')
   }
-  // function as property
-  start() {
-    this.engine.start()
-  }
-  // start = this.engine.start;
-  // stop = this.engine.stop
-}
 
-const car = new Car();
-// const car = new Car(new Engine());
-car.start()
+}
+const engine = new Engine();
+engine.start
+logl('engine start')(engine)
