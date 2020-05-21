cd0a61e08da315e1209bb12d48aedd553e0df172
lim byeong cheol <lim6112j@gmail.com>
2020-05-21 15:31:31 +0900

    functional minimizing

--- Promise/index.js
+++ Promise/index.js
@@ -17,41 +17,49 @@ const url = 'http://localhost:3000/users/';
 /**
  * nested callback chain
  */
-Axios(url)
-.then(results => {
-  const users=results.data;
-  users.sort((a,b) => 
-  a.id < b.id ? -1 
-  : a.id > b.id ? 1
-  : 0
-  );
-  // console.log(users);
-  for (let i = 0; i < users.length; i++) {
-    let user = users[i];
-    if(user.country === 'kr'){
-      Axios(`${url}${user.id}`)
-      .then(res => {
-        const user = res.data;
-        showUsers(user, user.grades) 
-      })
-      .catch(err => console.log(err))
-    }
-  }
-})
-.catch(err => console.log(err))
+// Axios(url)
+// .then(results => {
+//   const users=results.data;
+//   users.sort((a,b) => 
+//   a.id < b.id ? -1 
+//   : a.id > b.id ? 1
+//   : 0
+//   );
+//   // console.log(users);
+//   for (let i = 0; i < users.length; i++) {
+//     let user = users[i];
+//     if(user.country === 'kr'){
+//       Axios(`${url}${user.id}`)
+//       .then(res => {
+//         const user = res.data;
+//         showUsers(user, user.grades) 
+//       })
+//       .catch(err => console.log(err))
+//     }
+//   }
+// })
+// .catch(err => console.log(err))
 /**
  * nested callback chain with Promise Monad
  */
-// Axios.get(url)
-// .then(data => (data.data))
-// .then(R.filter(s => s.country === 'kr'))
-// .then(R.sortBy(R.prop('id')))
-// .then(R.map(user => {
-//     Axios.get(`${url}${user.id}`)
-//     .then(R.map(user => user.name ? showUsers(user, user.grades) : null))
-//   }
-// ))
-// .catch(err => console.log(err))
-// function showUsers(T, P) {
-//   console.log(T.name, " has won ", P, "points")
-// }
+function showUsers(T, P) {
+  console.log(T.name, " has won ", P, "points")
+}
+function run(...functions) {
+  return (initial) => {
+    return functions.reduce((prevReturn, fn) => fn(prevReturn), initial);
+  }
+}
+const fns = run (
+  data => data.data,
+  R.filter(s=> s.country === 'kr'),
+  R.sortBy(R.prop('id')),
+  R.map(user => {
+  Axios.get(`${url}${user.id}`).then(R.map(user => user.name ? showUsers(user, user.grades) : null))
+  }
+));
+
+
+Axios.get(url)
+.then(fns)
+.catch(err => console.log(err))
