58ca526b2ae26d4a499077e8d750a72b75d63fb3
lim byeong cheol <lim6112j@gmail.com>
2020-05-22 17:23:27 +0900

    generator funtion with raw function

--- function-generator/gen.ts
+++ function-generator/gen.ts
@@ -7,11 +7,30 @@ function* range(start=0, finish=Number.POSITIVE_INFINITY) {
     yield i;
   }
 }
+function rawRange(start: number, finish = Number.POSITIVE_INFINITY) {
+  return {
+    [Symbol.iterator]() {
+      return this;
+    },
+    next() {
+      if(start < finish) {
+        return { value: start++ , done: false};
+      }
+      return { done: true, value: finish };
+    }
+  }
+}
 // const num = range(1);
 // console.log(num.next().value);
 // console.log(num.next().value);
 // console.log(num.next().value);
-
+// const num = rawRange(1);
+// console.log(num.next().value);
+// console.log(num.next().value);
+// console.log(num.next().value);
+for(let x of rawRange(1, 3)) {
+  console.log(x)
+}
 /**
  * generator as iterator
  */
@@ -27,15 +46,15 @@ function* range(start=0, finish=Number.POSITIVE_INFINITY) {
 /**
  * take function
  */
-function take (amount: Number, generator: any): Number[] {
-  let result = [];
-  for(let n of generator) {
-    result.push(n);
-    if(n === amount) {
-      break;
-    }
-  }
-  return result;
-}
+// function take (amount: Number, generator: any): Number[] {
+//   let result = [];
+//   for(let n of generator) {
+//     result.push(n);
+//     if(n === amount) {
+//       break;
+//     }
+//   }
+//   return result;
+// }
 
-console.log(take(3, range(1, Infinity)))
+// console.log(take(3, range(1, Infinity)))
