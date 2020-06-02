37bfc7cb440206c6148321844aff28a86a441b9e
lim byeong cheol <lim6112j@gmail.com>
2020-06-02 09:46:16 +0900

    spread operator type

--- sort/sort.ts
+++ sort/sort.ts
@@ -1,4 +1,4 @@
-const quicksort = ([head, ...tail]) => head === undefined ? [] : 
+const quicksort = ([head, ...tail]: any[]):any => head === undefined ? [] : 
   [...quicksort([...tail.filter(a => a <= head)]), head, ...quicksort([...tail.filter(a => a > head)])];
 
 console.log(quicksort([2,4,1,7,3]))
