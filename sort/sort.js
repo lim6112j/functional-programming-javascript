97967b664e2d25834031ab489f81cd662bd303fd
lim byeong cheol <lim6112j@gmail.com>
2020-06-02 16:51:01 +0900

    mergemap working

--- sort/sort.js
+++ sort/sort.js
@@ -1,3 +1,4 @@
+"use strict";
 var __spreadArrays = (this && this.__spreadArrays) || function () {
     for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
     for (var r = Array(s), k = 0, i = 0; i < il; i++)
@@ -5,8 +6,17 @@ var __spreadArrays = (this && this.__spreadArrays) || function () {
             r[k] = a[j];
     return r;
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var lodash_1 = __importDefault(require("lodash"));
 var quicksort = function (_a) {
     var head = _a[0], tail = _a.slice(1);
     return head === undefined ? [] : __spreadArrays(quicksort(__spreadArrays(tail.filter(function (a) { return a <= head; }))), [head], quicksort(__spreadArrays(tail.filter(function (a) { return a > head; }))));
 };
 console.log(quicksort([2, 4, 1, 7, 3]));
+var mergeMap = function (arr) { return lodash_1.default(arr)
+    .map(function (v) { return [v]; })
+    .value(); };
+console.log(mergeMap([4, 2, 1, 5]));
--- sort/sort.ts
+++ sort/sort.ts
@@ -1,4 +1,10 @@
+import _ from 'lodash';
 const quicksort = ([head, ...tail]: any[]):any => head === undefined ? [] : 
   [...quicksort([...tail.filter(a => a <= head)]), head, ...quicksort([...tail.filter(a => a > head)])];
 
 console.log(quicksort([2,4,1,7,3]))
+
+const mergeMap = (arr: any[]) => _(arr)
+.map(v => [v])
+.value()
+console.log(mergeMap([4,2,1,5]))
