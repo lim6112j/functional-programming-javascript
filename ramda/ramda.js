01f539bc050dede693d3fdb13633358c8da9874e
lim byeong cheol <lim6112j@gmail.com>
2020-06-04 13:54:35 +0900

    lift

--- ramda/ramda.js
+++ ramda/ramda.js
@@ -10,3 +10,11 @@ var addM = R.lift(R.add);
 console.log(addM(Maybe.Just(3), Maybe.Just(5))); //=> Maybe.Just(8);
 
 console.log(addM([3], [5]));
+
+// lift
+var madd3a = R.curry((a,b,c) => a + b + c);
+var madd3 = R.lift(R.curry((a,b,c) => a + b+ c));
+var madd3N = R.liftN(3, R.curryN(3, (...args) => R.sum(args)))
+console.log(madd3a([1,2,3], [1,2,3], [1]))
+console.log(madd3([1,2,3], [1,2,3], [1]))
+console.log(madd3N([1,2,3], [1,2,3], [1]))
