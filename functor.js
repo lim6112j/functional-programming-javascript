878848732eebaccf963f470e965c978b3bafa90a
lim byeong cheol <lim6112j@gmail.com>
2020-06-15 17:08:12 +0900

    functor simplified

--- functor.js
+++ functor.js
@@ -3,9 +3,13 @@ export class Wrapper {
     this._value = value
   }
   map(f) {
-    return f(this._value)
+    return new Wrapper(f(this._value))
   }
   toString() {
     return 'Wrapper (' + this._value + ')'
   }
 }
+
+const obj = { name: 'iphone 12', price: 1200, discount: 20};
+const wrappedObj = new Wrapper(obj);
+wrappedObj.map(v => v.name).map(console.log);
