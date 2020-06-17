32e5e1e8e3517ceff8820677368203aa181d8de8
byeongcheollim <lim6112j@gmail.com>
2020-06-18 00:46:28 +0900

    quiz

---
+++ quiz/InvertBinary.js
@@ -0,0 +1,28 @@
+// Definition for a binary tree node.
+//      4
+//    /   \
+//   2     7
+//  / \   / \
+// 1   3 6   9
+//      4
+//    /   \
+//   7     2
+//  / \   / \
+// 9   6 3   1
+var TreeNode = /** @class */ (function () {
+    function TreeNode(val, left, right) {
+        this.val = (val === undefined ? 0 : val);
+        this.left = (left === undefined ? null : left);
+        this.right = (right === undefined ? null : right);
+    }
+    return TreeNode;
+}());
+function invertTree(root) {
+    if (root === null) {
+        return null;
+    }
+    return new TreeNode(root.val, invertTree(root.right), invertTree(root.left));
+}
+;
+var aTree = new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(7, new TreeNode(6), new TreeNode(9)));
+console.log(invertTree(aTree));
