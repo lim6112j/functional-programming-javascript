b4af3895f1694273889c662a446136693d460e5d
lim byeong cheol <lim6112j@gmail.com>
2020-06-17 19:15:30 +0900

    chain function

--- functor.js
+++ functor.js
@@ -8,8 +8,16 @@ export class Wrapper {
   toString() {
     return 'Wrapper (' + this._value + ')'
   }
+  chain(f) {
+    return f(this._value);
+  }
 }
 
 const obj = { name: 'iphone 12', price: 1200, discount: 20};
 const wrappedObj = new Wrapper(obj);
-wrappedObj.map(v => v.name).map(console.log);
+const f = (v) => v.name;
+wrappedObj.map(f).map(console.log);
+
+const fn = (v) => new Wrapper(v.name);
+
+wrappedObj.chain(fn).chain(console.log);
