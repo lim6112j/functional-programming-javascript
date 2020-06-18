8d4f5bb4248d4bf50988cb5a738c0791a21cb0c8
byeongcheollim <lim6112j@gmail.com>
2020-06-19 02:12:18 +0900

    citation

---
+++ quiz/citation.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} citations set to be of ascending order
+ * @return {number}
+ * [0] => 0
+ * [1] => 1
+ * [100] => 1
+ * [0, 1] => 1
+ * [0, 2] => 1
+ * [0, 1, 3, 5, 6] => 3
+ */
+var hIndex = function (citations) {
+    var len = citations.length;
+    var hh = function (h) {
+        if (h < 0)
+            return 0;
+        // console.log(citations.slice(h))
+        return citations[h] >= h + 1 ? citations[h] === 0 ? 0 : h + 1 : hh(h - 1);
+    };
+    return hh(len - 1);
+};
+console.log(hIndex([0]));
+console.log(hIndex([1]));
+console.log(hIndex([100]));
+console.log(hIndex([0, 1]));
+console.log(hIndex([0, 2]));
+console.log(hIndex([0, 1, 3, 5, 6]));
