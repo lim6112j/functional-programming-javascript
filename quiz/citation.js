e85dbcf848c4b10374ea4af346da6ad00335e47b
lim byeong cheol <lim6112j@gmail.com>
2020-06-19 12:56:11 +0900

    add sorting

--- quiz/citation.js
+++ quiz/citation.js
@@ -8,19 +8,25 @@
  * [0, 2] => 1
  * [0, 1, 3, 5, 6] => 3
  */
+var ascendOrd = function (list) {
+    return list.sort(function (a, b) { return a - b; });
+};
 var hIndex = function (citations) {
     var len = citations.length;
+    var asd = ascendOrd(citations);
     var hh = function (h) {
-        if (h < 0)
+        if (h === len)
             return 0;
-        return (citations[h] === citations.slice(h).length) ? citations.slice(h).length : hh(h - 1);
+        return citations[h] >= citations.slice(h).length ? citations.slice(h).length : hh(h + 1);
     };
-    return hh(len - 1);
+    return hh(0);
 };
-console.log(hIndex([0]));
-console.log(hIndex([1]));
-console.log(hIndex([100]));
-console.log(hIndex([0, 0]));
+console.log(hIndex([0])); // should 0
+console.log(hIndex([1])); // should 1
+console.log(hIndex([100])); // should 1
+console.log(hIndex([0, 0])); // should 0
 console.log(hIndex([0, 1])); // should 1
 console.log(hIndex([0, 2])); // should 1
-console.log(hIndex([0, 1, 3, 5, 6]));
+console.log(hIndex([11, 15])); // should 2
+console.log(hIndex([0, 1, 3, 5, 6])); // should 3
+console.log(hIndex([3, 0, 6, 1, 5]));
--- quiz/citation.ts
+++ quiz/citation.ts
@@ -8,19 +8,26 @@
  * [0, 2] => 1
  * [0, 1, 3, 5, 6] => 3
  */
+var ascendOrd = function (list: number[]): number[] {
+  return list.sort((a, b) => a -b)
+}
 var hIndex = function(citations: number[]):number {
     const len = citations.length
+    let asd = ascendOrd(citations);
     const hh = (h: number): number => {
-      if(h < 0) return 0
-      return (citations[h] === citations.slice(h).length ) ? citations.slice(h).length : hh(h-1)
+      if(h === len) return 0
+      return citations[h] >= citations.slice(h).length ? citations.slice(h).length : hh(h+1)
     }
     
-    return hh(len - 1)
+    return hh(0)
 };
-console.log(hIndex([0]))
-console.log(hIndex([1]))
-console.log(hIndex([100]))
-console.log(hIndex([0, 0]))
+
+console.log(hIndex([0])) // should 0
+console.log(hIndex([1])) // should 1
+console.log(hIndex([100])) // should 1
+console.log(hIndex([0, 0])) // should 0
 console.log(hIndex([0, 1])) // should 1
 console.log(hIndex([0, 2])) // should 1
-console.log(hIndex([0, 1, 3 ,5,6]))
+console.log(hIndex([11, 15])) // should 2
+console.log(hIndex([0, 1, 3 ,5,6])) // should 3
+console.log(hIndex([3,0,6,1,5]))
