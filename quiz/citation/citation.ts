c041a6b506df449bcf77a3f0ff6351c78e2c1224
byeongcheollim <lim6112j@gmail.com>
2020-06-21 10:43:03 +0900

    citation refactoring with web test

--- quiz/citation/citation.ts
+++ quiz/citation/citation.ts
@@ -8,6 +8,7 @@
  * [0, 2] => 1
  * [0, 1, 3, 5, 6] => 3
  */
+import { assert, header } from '../../utils/util';
 var ascendOrd = function (list: number[]): number[] {
   return list.sort((a, b) => a -b)
 }
@@ -62,3 +63,17 @@ console.log(hIndex([3,0,6,1,5])) // should 3
 console.log(hIndex([7,7,7,7,7,7,7])) // should 7
 console.log(hIndex([1, 4, 7, 9])) // should 3
 console.timeEnd('hindex')
+
+// test
+header("hIndex test starting")
+assert(hIndex([0])===0, "index([0]) should 0")
+assert(hIndex([1]) === 1, "index([1]) should 1")
+assert(hIndex([100])=== 1, "hIndex([100]) should 1")
+assert(hIndex([0,0])===0, "hIndex([0,0]) should 0")
+assert(hIndex([0,1])===1, "hIndex([0,1]) should 1")
+assert(hIndex([0, 2])===1, "hIndex([0, 2]) should 1")
+assert(hIndex([11, 15])===2, "hIndex([11, 15]) should 2")
+assert(hIndex([0, 1, 3 ,5,6])===3, "[0, 1, 3 ,5,6]) should 3")
+assert(hIndex([3,0,6,1,5])===3, "hIndex([3,0,6,1,5]) should 3")
+assert(hIndex([7,7,7,7,7,7,7])===7, "hIndex([7,7,7,7,7,7,7]) should 7")
+assert(hIndex([1, 4, 7, 9])===3, "hIndex([1, 4, 7, 9]) should 3")
