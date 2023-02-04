4096269aaed7ba183d67a37c1c5d5717d44e3633
lim6112j <lim6112j@gmail.com>
2023-02-05 00:57:03 +0900

    refactor

--- IOTest/index.js
+++ IOTest/index.js
@@ -1,16 +1,18 @@
-import _ from 'lodash'
-import { IO } from '../monad'
-const read = (document, selector) => () => document.querySelector(selector).innerHTML
+import _ from "lodash";
+import { IO } from "../monad";
+const read = (document, selector) => () =>
+  document.querySelector(selector).innerHTML;
 
 const write = (document, selector) => (val) => {
-    document.querySelector(selector).innerHTML = val
-    return val
-  }
+  document.querySelector(selector).innerHTML = val;
+  return val;
+};
 
-// partial fn with document 
-const readDom = _.partial(read, document)
-const writeDom = _.partial(write, document)
+// partial fn with document
+const readDom = _.partial(read, document);
+const writeDom = _.partial(write, document);
 
-const changeToStartCase =
-  IO.from(readDom('#read')).map(_.startCase).map(writeDom('#write'))
-changeToStartCase.run()
+const changeToStartCase = IO.from(readDom("#read"))
+  .map(_.startCase)
+  .map(writeDom("#write"));
+changeToStartCase.run();
